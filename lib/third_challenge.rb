50d948a04be7d87dc61fbef8619e8d1c6979029c
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-15 20:11:58 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,7 +3,7 @@ def third_challenge
    :montague => {
       :patriarch => {
         :name => "Lord Montague" 
-        :age =>
+        :age => 
         },
       :matriarch => {
         :name =>
